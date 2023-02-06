ebc1515d5667c1017fc2eaa8d0cc0cc4838434cf
qkr10 <qkrtngks10@naver.com>
2023-02-07 01:16:12 +0900

    api 권한 없는 경우 처리 방법 변경

---
+++ src/main/java/com/dalda/dalda_server/config/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.dalda.dalda_server.config.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        response.sendError(401);
+    }
+}
