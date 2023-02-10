793e093c821700b72cf87defb57a4f3c918ac3a9
qkr10 <qkrtngks10@naver.com>
2023-02-10 23:30:18 +0900

    설정수정 : 로그인 리다이렉트 수정

--- src/main/java/com/dalda/dalda_server/config/auth/OAuth2SuccessHandler.java
+++ src/main/java/com/dalda/dalda_server/config/auth/OAuth2SuccessHandler.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -19,13 +17,6 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
-        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
-
-        if(savedRequest!=null) {
-            String targetUrl = savedRequest.getRedirectUrl();
-            getRedirectStrategy().sendRedirect(request, response, targetUrl);
-        } else {
-            getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000");
-        }
+        getRedirectStrategy().sendRedirect(request, response, "http://dalda.xyz:3000");
     }
 }
