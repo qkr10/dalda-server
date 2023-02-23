286edd3fede69de7855d274f7bc9c331c4478f7b
qkr10 <qkrtngks10@naver.com>
2023-02-23 19:57:35 +0900

    리팩토링 : @RequestMapping 사용

--- src/main/java/com/dalda/dalda_server/web/CommentController.java
+++ src/main/java/com/dalda/dalda_server/web/CommentController.java
@@ -6,23 +6,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
     private final HttpServletRequest httpServletRequest;
 
-    @GetMapping("/comments")
+    @GetMapping
     public CommentsResponse comment() {
         String pageStr = httpServletRequest.getParameter("page");
         String sizeStr = httpServletRequest.getParameter("size");
         return commentService.findRootCommentListOrderByUpvote(pageStr, sizeStr);
     }
 
-    @GetMapping("/comments/{rootId}")
+    @GetMapping("/{rootId}")
     public CommentsResponse comment(@PathVariable("rootId") Long rootId) {
         String pageStr = httpServletRequest.getParameter("page");
         String sizeStr = httpServletRequest.getParameter("size");
