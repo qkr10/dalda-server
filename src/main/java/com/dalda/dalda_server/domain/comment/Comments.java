14addf4d96775a20fb440751ffd9a60e912ec0c9
qkr10 <qkrtngks10@naver.com>
2023-03-16 14:42:41 +0900

    기능수정 : comments entity "content" 수정(default : none)

--- src/main/java/com/dalda/dalda_server/domain/comment/Comments.java
+++ src/main/java/com/dalda/dalda_server/domain/comment/Comments.java
@@ -38,7 +38,7 @@ public class Comments extends BaseTimeEntity {
     @JoinColumn(name = "mention_user_id")
     private Users mentionUser;
 
-    @Column(nullable = false, columnDefinition = "TEXT default ''")
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(nullable = false)
