83c42fd1b0668ec8a01c9ca2944cb5a6e0525a0f
qkr10 <qkrtngks10@naver.com>
2023-03-16 21:18:07 +0900

    버그수정 : cors에러 수정

--- src/main/java/com/dalda/dalda_server/config/CORSConfig.java
+++ src/main/java/com/dalda/dalda_server/config/CORSConfig.java
@@ -1,5 +1,6 @@
 package com.dalda.dalda_server.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,10 +13,13 @@ public class CORSConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("http://dalda.xyz:3000", "http://dalda.xyz")
+                        .allowedOrigins("http://dalda.xyz:3000",
+                                "http://dalda.xyz",
+                                "http://front.dalda.xyz",
+                                "http://front.dalda.xyz:3000")
                         .allowCredentials(true);
             }
         };
