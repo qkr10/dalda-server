7468d7dbf8d79cffd0a440bb4b8837c5cf2a8a32
qkr10 <qkrtngks10@naver.com>
2023-04-04 23:18:56 +0900

    기능수정 : 인증실패로 에러발생시 바디에 json 메세지 넣기

--- src/main/java/com/dalda/dalda_server/config/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/dalda/dalda_server/config/auth/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,9 @@
 package com.dalda.dalda_server.config.auth;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -9,7 +11,9 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 @Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws ServletException, IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        request.getRequestDispatcher("/error").forward(request, response);
     }
 }
--- src/main/java/com/dalda/dalda_server/web/IndexController.java
+++ src/main/java/com/dalda/dalda_server/web/IndexController.java
@@ -1,5 +1,9 @@
 package com.dalda.dalda_server.web;
 
+import com.dalda.dalda_server.web.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +13,12 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "";
+    }
+
+    @GetMapping("/error")
+    public ErrorResponse error(HttpServletRequest request, HttpServletResponse response) {
+        int code = response.getStatus();
+        return new ErrorResponse(code, HttpStatusCode.valueOf(code).toString());
     }
 
 }
