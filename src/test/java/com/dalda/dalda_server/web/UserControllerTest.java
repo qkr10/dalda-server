be257e8b3591de0fcc1402adeee42832437acefe
qkr10 <qkrtngks10@naver.com>
2023-04-27 12:26:14 +0900

    리팩토링 : before/after each 를 사용

--- src/test/java/com/dalda/dalda_server/web/UserControllerTest.java
+++ src/test/java/com/dalda/dalda_server/web/UserControllerTest.java
@@ -2,14 +2,19 @@ package com.dalda.dalda_server.web;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dalda.dalda_server.config.auth.dto.UserPrincipal;
 import com.dalda.dalda_server.domain.user.Role.Role;
 import com.dalda.dalda_server.domain.user.UserRepository;
 import com.dalda.dalda_server.domain.user.Users;
+import com.dalda.dalda_server.web.response.UserResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
 import java.util.HashMap;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,14 +27,15 @@ import org.springframework.test.web.servlet.MockMvc;
 public class UserControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
-
+    private MockMvc mockMvc;
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
-    @Test
-    public void testMyInfo() throws Exception {
-        Users user = Users.builder()
+    private Users user;
+
+    @BeforeEach
+    public void beforeAll() {
+        user = Users.builder()
                 .name("test")
                 .email("test@test.test")
                 .role(Role.USER)
@@ -37,13 +43,46 @@ public class UserControllerTest {
                 .picture("asdfasdf")
                 .build();
 
-        UserPrincipal principal = new UserPrincipal(
+        userRepository.save(user);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        userRepository.delete(user);
+    }
+
+    @Test
+    public void testAuthMe() throws Exception {
+        //request
+        var requestURL = "/auth/me";
+        var requestPrincipal = new UserPrincipal(
                 user,
                 new HashMap<String, Object>(),
                 Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())));
 
-        mockMvc.perform(get("/auth/me").with(oauth2Login().oauth2User(principal)))
-                .andExpect(status().isOk());
+        //response
+        var responseUser = new UserResponse(user);
+        var responseBody = new ObjectMapper().writeValueAsString(responseUser);
+
+        //perform
+        mockMvc.perform(get(requestURL).with(oauth2Login().oauth2User(requestPrincipal)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody));
+    }
+
+    @Test
+    public void testUsersHandle() throws Exception {
+        //request
+        var requestURL = "/users/" + user.getHandle();
+
+        //response
+        var responseUser = new UserResponse(user);
+        var responseBody = new ObjectMapper().writeValueAsString(responseUser);
+
+        //perform
+        mockMvc.perform(get(requestURL))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody));
     }
 
 }
